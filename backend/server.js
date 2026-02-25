2a394fa2d5b3c3e0914deaca0d196fbf0363d8d2
KassiyetB <kaszi.exe@gmail.com>
2026-02-25 16:47:19 +0500

    configure postgreSQL

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
+const chalk = require('chalk');
 const app = require('./src/app');
-const PORT = 3000;
+const pool = require('./src/config/db');
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+
+pool.connect()
+.then(() => {
+    console.log(chalk.green.bold("Connected to PostgreSQL"));
+    app.listen(PORT, () => {
+        console.log(chalk.green(`Server is running on ${chalk.blue.underline('http://localhost:' + chalk.bold(PORT))}`));
+    });
+})
+.catch(err => {
+    console.error(chalk.red("Database connection error"), err);
+})
+
