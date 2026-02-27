e97dda7fcd5abba8588948356a547e4315d32f0e
KassiyetB <kaszi.exe@gmail.com>
2026-02-27 10:06:34 +0500

    set up default css styles

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return(
+        <>
+            <h1>This is header1 </h1>
+            <p>This is normal text</p>
+            <p>бұл қалыпты мәтін</p>
+            <p className="gray-text">This is gray text</p>
+            <input placeholder="This is input"></input><br/>
+            <a>This is anchor</a><br/>
+            <button>Continue</button>
+        </>
+    )
+}
--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "@/pages/Login";
 import MainLayout from "@/layouts/MainLayout";
 import Dashboard from "@/pages/Dashboard";
+import Home from "@/pages/Home";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 {/* Public Route */}
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login/>} />
 
                 {/* Protected Layout Routes */}
