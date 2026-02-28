1f71a84ca483226442b68bea6d1aea35f8fe820c
KassiyetB <kaszi.exe@gmail.com>
2026-02-28 19:38:38 +0500

    update country code selection with react-select

--- frontend/src/pages/Signup/Step2.tsx
+++ frontend/src/pages/Signup/Step2.tsx
@@ -1,33 +1,32 @@
 import { useEffect, useState } from 'react'
 import './Signup.css'
+import Select from 'react-select';
+
+type CountryOption = {
+        value: string; // The full phone code (e.g., +1)
+        label: string; // The country name
+        flag: string;  // The flag image URL
+    }
 
 export default function Step2() {
 
-    type CountryOption = {
-        name:string,
-        dialCode: string,
-        flag: string,
-    }
-
     const [countries, setCountries] = useState<CountryOption[]>([]);
+    const [selected, setSelected] = useState<CountryOption | null>(null);
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all?fields=name,idd,flags')
         .then(res=> res.json())
         .then((data: any[]) => {
             const formatted: CountryOption[] = data.map(country => ({
-                name: country.name.common,
-                dialCode: country.idd.root,
+                value: country.idd?.root,
+                label: country.name.common,
                 flag: country.flags.png,
-            })).sort((a, b) => a.name.localeCompare(b.name));
+            }))
+            .sort((a, b) => a.label.localeCompare(b.label));
             setCountries(formatted);
             return formatted;
-        })
-
-        
-    }, [])
-
-    console.log(countries);
+        });
+    }, []);
 
     return(
         <form className='Signup-form'>
@@ -35,17 +34,21 @@ export default function Step2() {
             <p>Please confirm your country code and enter your phone number</p> 
             <div id="s-input-1" className="signup-input-phone">
                 <hr />
-                <select>
-                    {countries.map((country: CountryOption) => (
-                        <option>
-                            <img className='country-flag' src={country?.flag}/>
-                            <p className='country-name'>{country?.name}</p>
-                        </option>
-                ))}
-                </select>
+                <Select 
+                    options={countries}
+                    value={selected}
+                    onChange={(option) => setSelected(option)}
+                    formatOptionLabel={country => (
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <img src={country.flag} alt="" style={{ width: 20, marginRight: 10 }} />
+                            <span>{country.label}</span>
+                        </div>
+                    )}
+
+                />
                 <hr />
-                <div>
-                    <p className='country-code'>{countries[114]?.dialCode}</p>
+                <div style={{display: "flex", gap:"15px", alignItems:"center"}}>
+                    <p className='country-code'>{selected?.value}</p>
                     <div className='vertical-line'></div>
                     <input type='text' className='phone-number' inputMode='numeric' pattern='[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}' placeholder='000 000 00 00'></input>
                 </div>
